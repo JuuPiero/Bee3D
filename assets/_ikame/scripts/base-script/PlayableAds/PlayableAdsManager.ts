d4d99b49d9a80097f02f43ca9910567c0473325e
ChuongPT <chuongpt@ikameglobal.com>
2026-01-20 18:43:07 +0700

    add link IOS

--- assets/_ikame/scripts/base-script/PlayableAds/PlayableAdsManager.ts
+++ assets/_ikame/scripts/base-script/PlayableAds/PlayableAdsManager.ts
@@ -9,7 +9,7 @@ import { EventName } from '../../designPatterns/observer/EventName';
 const { ccclass, property } = _decorator;
 
 const urlPlayStore: string = "https://play.google.com/store/apps/details?id=com.ig.water.flow";
-const urlAppStore: string = "https://apps.apple.com"
+const urlAppStore: string = "https://apps.apple.com/vn/app/color-flow-shooter/id6756484165";
 
 @ccclass('PlayableAdsManager')
 export class PlayableAdsManager extends SingletonInSceneComponent {
