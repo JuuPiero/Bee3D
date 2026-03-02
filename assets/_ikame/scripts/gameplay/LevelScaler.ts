b30c34c898d3b6a6c2b705d4144466bdf26122a7
ChuongPT <chuongpt@ikameglobal.com>
2026-03-02 08:56:30 +0700

    rescale the level

--- assets/_ikame/scripts/gameplay/LevelScaler.ts
+++ assets/_ikame/scripts/gameplay/LevelScaler.ts
@@ -14,7 +14,7 @@ export class LevelScaler extends Component
         const ratio = this.canvasRect.contentSize.width / this.canvasRect.contentSize.height;
         const orthoWidth = this.mainCamera.orthoHeight * ratio;
         // Chỉ scale khi màn hình hẹp hơn 16:9, ngược lại giữ nguyên
-        const newObjectScale = ratio < (9 / 16) ? (orthoWidth * 2) / OG_SIZE : 1;
+        const newObjectScale = ratio <= (9 / 16) ? (orthoWidth * 2) / OG_SIZE : 1;
         this.node.setScale(newObjectScale, newObjectScale, newObjectScale);
     }
 }
