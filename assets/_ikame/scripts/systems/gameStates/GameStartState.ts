67bfed046adf699062f1f86bea424628743b5644
ChuongPT <chuongpt@ikameglobal.com>
2026-05-13 17:24:52 +0700

    add event challenge start

--- assets/_ikame/scripts/systems/gameStates/GameStartState.ts
+++ assets/_ikame/scripts/systems/gameStates/GameStartState.ts
@@ -1,4 +1,4 @@
-import { _decorator, game } from 'cc';
+import { _decorator } from 'cc';
 import { GameStateBase } from './GameStateBase';
 import { EGameState } from './EGameState';
 import { IChangeState } from '../../designPatterns/stateMachine/BaseStateMachine';
@@ -7,36 +7,32 @@ import { ILevelController } from '../../gameplay/controllers/ILevelController';
 import { ETrackingEvent, TrackingManager } from '../../base-script/PlayableAds/Tracking/TrackingManager';
 
 export class GameStartState extends GameStateBase {
-    
+
     private levelController: ILevelController;
 
-    constructor(name: EGameState, stateMachine: IChangeState<EGameState>, levelController: ILevelController)
-    {
+    constructor(name: EGameState, stateMachine: IChangeState<EGameState>, levelController: ILevelController) {
         super(name, stateMachine);
         this.levelController = levelController;
     }
 
-    public onEnter(): void
-    {
+    public onEnter(): void {
         super.onEnter();
         this.setup();
     }
 
-    public async setup() 
-    {
-        try
-        {
+    public async setup() {
+        try {
             TrackingManager.TrackEvent(ETrackingEvent.LOADING);
             this.levelController.clearLevel();
             // this.levelController.scaleLevel();
             TrackingManager.TrackEvent(ETrackingEvent.LOADED);
             TrackingManager.TrackEvent(ETrackingEvent.DISPLAYED);
             this.levelController.spawnLevel();
+            TrackingManager.TrackEvent(ETrackingEvent.CHALLENGE_STARTED);
             await PromiseDelay.Wait(1.5);
             this.stateMachine.changeState(EGameState.Idle);
         }
-        catch (error)
-        {
+        catch (error) {
             console.error("Error during GameStartState setup:", error);
         }
     }
