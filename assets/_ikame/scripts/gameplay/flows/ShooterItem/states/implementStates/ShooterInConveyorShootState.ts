8febf8f3629640c90fa727e63ceee43e9b3f340d
ChuongPT <chuongpt@ikameglobal.com>
2026-06-11 16:21:14 +0700

    fix the color burn

--- assets/_ikame/scripts/gameplay/flows/ShooterItem/states/implementStates/ShooterInConveyorShootState.ts
+++ assets/_ikame/scripts/gameplay/flows/ShooterItem/states/implementStates/ShooterInConveyorShootState.ts
@@ -5,7 +5,7 @@ import { ShooterAnimationName } from "../ShooterAnimationName";
 import { ShooterStateBase } from "../ShooterStateBase";
 import { game } from "cc";
 
-
+const FIRE_INTERVAL = 0.02
 export class ShooterInConveyorShootState extends ShooterStateBase {
 
     private _target: IPixelBlock;
@@ -34,7 +34,7 @@ export class ShooterInConveyorShootState extends ShooterStateBase {
                 this._shooter.pauseAnimation();
             await this._shooter.rotateTowardsTargetAsync(angle);
             this._shooter.changeAnimation(ShooterAnimationName.Attack, true);
-            // const delayTime = Math.max(0, FIRE_INTERVAL - (game.totalTime - this._lastFireTime) * 1000);
+            const delayTime = Math.max(0, FIRE_INTERVAL - (game.totalTime - this._lastFireTime) * 1000);
             // await PromiseDelay.Wait(delayTime);
             this._shooter.shootTarget(this._target);
             this._lastFireTime = game.totalTime;
