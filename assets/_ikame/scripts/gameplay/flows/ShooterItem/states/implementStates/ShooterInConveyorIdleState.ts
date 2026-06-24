9f72f096e73e0d098f71965300865afe8e29f44b
ChuongPT <chuongpt@ikameglobal.com>
2026-06-24 13:49:42 +0700

    optimize update loop

--- assets/_ikame/scripts/gameplay/flows/ShooterItem/states/implementStates/ShooterInConveyorIdleState.ts
+++ assets/_ikame/scripts/gameplay/flows/ShooterItem/states/implementStates/ShooterInConveyorIdleState.ts
@@ -2,9 +2,22 @@ import { PromiseDelay } from "db://assets/_ikame/scripts/commons/PromiseDelay";
 import { ShooterAnimationName } from "../ShooterAnimationName";
 import { ShooterStateBase } from "../ShooterStateBase";
 import { EShooterState } from "../EShooterState";
+import { IShooterItem } from "../../IShooterItem";
+import { IChangeState } from "db://assets/_ikame/scripts/designPatterns/stateMachine/BaseStateMachine";
 
 export class ShooterInConveyorIdleState extends ShooterStateBase
 {
+    private static FRAME_SIGN: boolean = false;
+    private _frameSign: boolean;
+    private _frameCount: number = -1;
+
+    constructor(stateName: EShooterState, stateMachine: IChangeState<EShooterState>, shooter: IShooterItem)
+    {
+        super(stateName, stateMachine, shooter);
+        ShooterInConveyorIdleState.FRAME_SIGN = !ShooterInConveyorIdleState.FRAME_SIGN;
+        this._frameSign = ShooterInConveyorIdleState.FRAME_SIGN;
+
+    }
 
     public onEnter(): void
     {
@@ -15,18 +28,23 @@ export class ShooterInConveyorIdleState extends ShooterStateBase
 
     public onUpdate(dt: number): void
     {
-        const targetCount = this._shooter.tryShootTargets();
-        if (targetCount > 0)
+        this._frameCount = (this._frameCount + 1) % 2;
+        const alowUpdate = (this._frameCount === 0) === this._frameSign;
+
+        if (alowUpdate)
         {
-            this._shooter.changeAnimation(ShooterAnimationName.Attack, true);
-            // this._shooter.reduceAmmoCount(targetCount);
-            this._shooter.shootSoundEffect();
-            if (this._shooter.getAmmoCount() <= 0)
+            const targetCount = this._shooter.tryShootTargets();
+            if (targetCount > 0)
             {
-                this.outOfAmmoRoutine();
+                this._shooter.changeAnimation(ShooterAnimationName.Attack, true);
+                this._shooter.shootSoundEffect();
+                if (this._shooter.getAmmoCount() <= 0)
+                {
+                    this.outOfAmmoRoutine();
+                }
             }
         }
-        // if (this._shooter.getAmmoCount() > 0)
+
         this._shooter.moveAlongConveyor(dt);
     }
 
