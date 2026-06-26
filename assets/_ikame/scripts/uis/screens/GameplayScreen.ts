8416850a92a18020222f05a900fe972130e2b6c8
ChuongPT <chuongpt@ikameglobal.com>
2026-06-26 11:33:09 +0700

    build level theme mua thu

--- assets/_ikame/scripts/uis/screens/GameplayScreen.ts
+++ assets/_ikame/scripts/uis/screens/GameplayScreen.ts
@@ -7,32 +7,5 @@ const { ccclass, property } = _decorator;
 @ccclass('GameplayScreen')
 export class GameplayScreen extends ScreenBase
 {
-    @property(Widget)
-    public playButton: Widget = null;
-    
-    onShow(): void
-    {
-        super.onShow();
-
-        this.scheduleOnce(() =>
-        {
-            this.playButton.bottom = -260;
-            this.playButton.node.active = true; 
-            tween(this.playButton)
-                .to(0.5, { bottom: 70 }, { easing: 'sineOut' })
-                .start();
-        }, 5);
-        if (TO_STORE_AFTER_PLAYNOW)
-        {
-            this.scheduleOnce (() => {
-                PlayableAdsManager.Instance().ForceOpenStore();
-            }, 10);
-        }
-    }
-
-    onHide(): void
-    {
-        super.onHide();
-        this.unscheduleAllCallbacks();
-    }
+   
 }
